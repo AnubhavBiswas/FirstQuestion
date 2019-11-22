031ca6e0d747945d1db2978c1b08866ea18bbc5d
AnubhavBiswas <ajack9066@gmail.com>
2019-11-23 01:05:03 +0530

    ninth question

---
+++ Question9.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Question9 {
+
+    public static void main(String[] args) {
+        Scanner s1=new Scanner(System.in);
+        int n=s1.nextInt();
+        int arr[]=new int[n];
+        int k=s1.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=s1.nextInt();
+        }
+        int c=arr[0];
+        System.out.println("Max count is:");
+        for(int i=k;i<n;i++)
+        {
+            for(int j=0;j<=i;j++)
+            {
+                if(c<arr[j])
+                {
+                    c=arr[j];
+                }
+            }
+            System.out.println(c);
+            c=arr[0];
+        }
+    }
+
+}
